0dffb4174fadbc68e52bf845bf1661e0c0473a62
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-07-27 20:08:37 -0300

    Atualizado api de ping app para .NET 6, adicionado nova configuração ao program e removido classe startup do projeto

--- PingIp/Ping.Ip.Web/Program.cs
+++ PingIp/Ping.Ip.Web/Program.cs
@@ -1,20 +1,100 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Ping.Ip.App.Service;
+using Ping.Ip.Domain;
+using Ping.Ip.Domain.Service;
+using Ping.Ip.Infra.Repository;
+using System.Text;
 
-namespace Ping.Ip.Web
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDispositivoRepository, DispositivoRepository>();
+builder.Services.AddScoped<IDispositivoService, DispositivoService>();
+
+ConfigurationManager Configuration = builder.Configuration;
+
+var key = Encoding.ASCII.GetBytes(Configuration["ChavePrivada"]);
+builder.Services.AddAuthentication(x =>
 {
-    public class Program
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod())
+);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ping.Ip.Web", Version = "v1" });
+
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "Autenticação baseada em Json Web Token (JWT)",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securitySchema);
+
+    var securityRequirement = new OpenApiSecurityRequirement
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+    c.AddSecurityRequirement(securityRequirement);
+});
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ping.Ip.Web v1"));
 }
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.Run();
