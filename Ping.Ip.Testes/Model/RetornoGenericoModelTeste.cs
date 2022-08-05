b9f2e0cf652a187d18756fac4c443303be79c6ab
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-08-05 18:30:53 -0300

    Add testes para a classe de retorno genérico, testando os construtores

---
+++ Ping.Ip.Testes/Model/RetornoGenericoModelTeste.cs
@@ -0,0 +1,42 @@
+﻿using Ping.Ip.Domain.Model;
+using Shouldly;
+using Xunit;
+
+namespace Ping.Ip.Testes.Model
+{
+    public class RetornoGenericoModelTeste
+    {
+        [Theory(DisplayName = "Testando construtor de sucesso")]
+        [InlineData(10, null)]
+        [InlineData(15, "Mensagem de teste ")]
+        public void DadoSucesso_ChamaConstrutorSucesso_RetornaModeloSucesso(int model, string mensagemSucesso)
+        {
+            // Arrange
+            var modelo = model;
+            var mensagem = mensagemSucesso;
+
+            // Act
+            var resultado = new RetornoGenericoModel<int>(model, mensagem);
+
+            // Assert
+            resultado.Status.ShouldBe(true);
+            resultado.Modelo.ShouldBe(modelo);
+            resultado.Mensagem.ShouldBe(mensagem);
+        }
+
+        [Fact(DisplayName = "Testando construtor de erro com mensagem")]
+        public void DadoSucesso_ChamaConstrutorSucesso_RetornaModeloErroComMensagem()
+        {
+            // Arrange
+            var mensagem = "Mensagem de erro";
+
+            // Act
+            var resultado = new RetornoGenericoModel<int>(mensagem);
+
+            // Assert
+            resultado.Status.ShouldBe(false);
+            resultado.Modelo.ShouldBe(0);
+            resultado.Mensagem.ShouldBe(mensagem);
+        }
+    }
+}
