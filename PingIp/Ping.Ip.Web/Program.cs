c270e3d20b4560065fbeee383494a6d8ae7da040
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-08-06 18:50:25 -0300

    Add configuração startup para fluent validation

--- PingIp/Ping.Ip.Web/Program.cs
+++ PingIp/Ping.Ip.Web/Program.cs
@@ -1,9 +1,7 @@
-using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
